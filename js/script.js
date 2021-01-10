fddb21318e10523cd522764b71e9fdf063b17706
reanimator0033 <76180888+reanimator0033@users.noreply.github.com>
2021-01-10 21:56:40 +0100

    www
    www

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick({
+    $('.carusel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                    dots: true,
+                    dots: false,
                     arrows: false
                 }
             }
         ]
     });
+   
+
+   
     
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
