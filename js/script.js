559a4b9b351feb2f4d22eeb90e2181a89c510608
reanimator0033 <76180888+reanimator0033@users.noreply.github.com>
2021-01-11 21:40:10 +0100

    wewe
    wewe

--- js/script.js
+++ js/script.js
@@ -105,4 +105,23 @@ $(document).ready(function(){
         });
         return false;
     });
+
+    //Scroll up
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>1400) {
+          $('.pageup').fadeIn();  
+        } else{
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
+
+    new WOW().init();
+
 });
